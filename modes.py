8b7834ffad177ae8a04d7d4cea4c1141172c0a45
syao <870114339@qq.com>
2018-10-28 17:41:34 +0800

    step_0_done

--- modes.py
+++ modes.py
@@ -24,8 +24,10 @@ def mode_f(filename, n=0):
     ''' - filename: .txt to read
         - n: return the first n words, 0 returns all
     '''
+    words = []
     with open(filename, 'r', encoding='utf-8') as f:
-        words = get_words(f.read())
+        for line in f:
+            words.extend(get_words(line))
     word_freq = nltk.FreqDist(words)
     word_freq = sorted(word_freq.items(), key=lambda item:item[1], reverse=True)
     if n == 0:
@@ -38,7 +40,7 @@ def mode_f(filename, n=0):
             print(str(key) + ':' + str(val))
             count += 1
 
-def mode_d(directory, is_recursive, n=0):
+
     
 
 
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 
-def get_words(text): return re.findall('[a-z]+[0-9]+', text.lower())
+def get_words(text): return re.findall('[a-z]+[0-9a-z]*', text.lower())
 
 def get_stopwords(stop_words_file):
     with open(stop_words_file, 'r', encoding='utf-8') as f:
